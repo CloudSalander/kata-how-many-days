686cb3894cac2cd5b13e6139978708628cf46e83
Cloud <ruben.as532@gmail.com>
2024-06-27 11:05:22 +0200

    What I did in 30 minutes

--- index.php
+++ index.php
@@ -12,9 +12,24 @@ function getDaysDifference(string $date1, string $date2): int {
     return floor($days_difference / SECONDS_IN_A_DAY);
 }
 
-$date1 = "18-09-2024";
-$date2 = "20-09-2024";
+function isValidDate(string $date): bool {
+    /*TODO: This should be a regex, but I'm not expert on regex. 
+      I'm sure all of us can search how to do it in ChatGPT so i will try a workaround */
+      var_dump($date);
+    return strlen($date) == 10 && $date[2] == "/" && $date[5] == "/" && isValidDateFormat($date);
+}
 
-echo "Entre ".$date1." i ".$date2." hi ha ".getDaysDifference($date1,$date2)." dia(es) de diferència";
+function isValidDateFormat(string $date): bool {
+    $date_parts = explode("/",$date);
+    //TODO: Validate day,month,year range...
+    //if(int($date_parts[0])
+    return false;
+}
 
+$date1 = readline("Enter 1 date(dd/mm/yyyy)");
+$date2 = readline("Another one!(dd/mm/yyyy)");
+
+if(isValidDate($date1) && isValidDate($date2)) {
+    echo "Entre ".$date1." i ".$date2." hi ha ".getDaysDifference($date1,$date2)." dia(es) de diferència";
+}
 ?>
